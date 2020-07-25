9979a0a7e160d647cc1218171a7c2942e78821bc
samuelokediji <samuelolaideokediji@gmail.com>
2020-07-25 01:07:58 +0100

    ideate gatsby blog added

---
+++ ideate-blog/src/pages/index.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Button from "../components/button"
+
+class IndexPage extends React.Component {
+  render() {
+    const siteTitle = " Ideate Gatsby Starter Blog"
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="Home"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+       
+        <img style={{ margin: 0 }} src="https://res.cloudinary.com/unitech-global-resource/image/upload/v1595118073/ID8_lmo8lb.png" alt="" />
+        <h1>
+          Hey people{" "}
+          <span role="img" aria-label="wave emoji">
+            👋
+          </span>
+        </h1>
+            
+        <p>Welcome to Ideate Blog website.</p>
+        <p>
+         Learn about how design meets branding
+        </p>
+        <p>Now go build something great!</p>
+        <Link to="/blog/">
+          <Button marginTop="35px">Go to Blog</Button>
+        </Link>
+      </Layout>
+    )
+  }
+}
+
+export default IndexPage
